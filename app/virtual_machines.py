042989ac3928b4ac7dab89fbd9ffaf0a88694345
Avi Nehama <avi.nehama@gmail.com>
2021-08-18 07:42:46 +0000

    implements CRD for self-served environments

--- app/virtual_machines.py
+++ app/virtual_machines.py
@@ -1,4 +1,3 @@
-import requests
 from flask import (
     current_app,
     Blueprint,
@@ -9,43 +8,107 @@ from flask import (
     render_template,
 )
 
-import pulumi 
 import pulumi_azure_native as azure_native
+import pulumi.automation as auto
+import pulumi
+
 
 bp = Blueprint("virtual_machines", __name__, url_prefix="/vms")
 
-def create_pulumi_program():
-    resource_group = azure_native.resources.ResourceGroup("resourceGroup", location="eastus",  resource_group_name="myResourceGroup")
+LOCATION = "eastus"
 
+def create_pulumi_program(env_name: str):
+    resource_group = azure_native.resources.ResourceGroup("resourceGroup", location=LOCATION,  resource_group_name=env_name)
+    storage_account = azure_native.storage.StorageAccount("storageAccount",
+        account_name="livecloudforteamssa",
+        kind="StorageV2",
+        location="eastus",
+        resource_group_name=resource_group.name,
+        sku=azure_native.storage.SkuArgs(
+          name="Standard_GRS",
+        ),
+    )
 
 @bp.route("/", methods=["GET"])
 def list_vms():
     vms = []
-    return render_template("virtual_machines/index.html")
+    org_name = current_app.config["PULUMI_ORG"]
+   
+    project_name = current_app.config["PROJECT_NAME"]
+    try:
+        ws = auto.LocalWorkspace(
+            project_settings=auto.ProjectSettings(name=project_name, runtime="python")
+        )
+        all_stacks = ws.list_stacks()
+        for stack in all_stacks:
+            stack = auto.select_stack(
+                stack_name=stack.name,
+                project_name=project_name,
+                # no-op program, just to get outputs
+                program=lambda: None,
+            )
+            outs = stack.outputs()
+            vms.append(
+                {
+                    "name": stack.name,
+                }
+            )
+
+    except Exception as exn:
+        flash(f"{str(exn)}", category="danger") 
+
+
+    return render_template("virtual_machines/index.html", vms=vms)
 
 @bp.route("/new", methods=["GET", "POST"])
 def create_vm():
     """creates new vm"""
     if request.method == "POST":
-        def pulumi_program():
-            return create_pulumi_program()
+        stack_name = request.form.get("site-id")
 
-    # try:
-    #     # create a new stack, generating our pulumi program on the fly from the POST body
-    #     stack = auto.create_stack(
-    #         stack_name=str(stack_name),
-    #         project_name=current_app.config["PROJECT_NAME"],
-    #         program=pulumi_program,
-    #     )
-    #     stack.set_config("aws:region", auto.ConfigValue("us-west-2"))
-    #     # deploy the stack, tailing the logs to stdout
-    #     stack.up(on_output=print)
-    #     flash(f"Successfully created site '{stack_name}'", category="success")
-    # except auto.StackAlreadyExistsError:
-    #     flash(
-    #         f"Error: Site with name '{stack_name}' already exists, pick a unique name",
-    #         category="danger",
-    #     )
+        def pulumi_program():
+            return create_pulumi_program(str(stack_name)) 
+    
+        try:
+            # create a new stack, generating our pulumi program on the fly from the POST body
+            stack = auto.create_stack(
+                stack_name=str(stack_name),
+                project_name=current_app.config["PROJECT_NAME"],
+                program=pulumi_program,
+            )
+            stack.set_config("aws:region", auto.ConfigValue("us-west-2"))
+            # deploy the stack, tailing the logs to stdout
+            stack.up(on_output=print)
+            flash(f"Successfully created environment '{stack_name}'", category="success")
+        except auto.StackAlreadyExistsError:
+            flash(
+                f"Error: Environment with name '{stack_name}' already exists, pick a unique name",
+                category="danger",
+            )
+
+        return redirect(url_for("virtual_machines.list_vms"))
+    return render_template("virtual_machines/create.html")
+
+
+@bp.route("/<string:id>/delete", methods=["POST"])
+def delete_vm(id: str):
+    stack_name = id
+    try:
+        stack = auto.select_stack(
+            stack_name=stack_name,
+            project_name=current_app.config["PROJECT_NAME"],
+            # noop program for destroy
+            program=lambda: None,
+        )
+        stack.destroy(on_output=print)
+        stack.workspace.remove_stack(stack_name)
+        flash(f"Site '{stack_name}' successfully deleted!", category="success")
+    except auto.ConcurrentUpdateError:
+        flash(
+            f"Error: Site '{stack_name}' already has update in progress",
+            category="danger",
+        )
+    except Exception as exn:
+        flash(str(exn), category="danger")
 
     return redirect(url_for("virtual_machines.list_vms"))
-
